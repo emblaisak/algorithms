61dbb16e5484054f8a19d06f1f32d5005b8279b6
Imre Mathias <imremathias@outlook.com>
2021-08-27 14:34:03 +0200

    ADD MyArrayListInteger EX01-B

--- exercises/src/main/java/org/pg4200/ex01/MyArrayListInteger.java
+++ exercises/src/main/java/org/pg4200/ex01/MyArrayListInteger.java
@@ -1,4 +1,33 @@
 package org.pg4200.ex01;
 
 public class MyArrayListInteger {
+    private Integer[] integerList;
+    private int size;
+
+    public  MyArrayListInteger(int maxSize) {
+        integerList = new Integer[maxSize];
+    }
+
+    // Call the other constructor
+    public MyArrayListInteger() {
+        this(10);
+    }
+
+    public Integer get(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index does not exist.");
+            return null;
+        }
+        return integerList[index];
+    }
+
+    public void add(Integer value) {
+        // Size-value because that number is the last index
+        integerList[size] = value;
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
