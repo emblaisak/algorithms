4ccacc43a69e6dd9fe186efb7a9315724f4ce3ee
Imre Mathias <imremathias@outlook.com>
2021-10-07 16:27:27 +0200

    EX 02-A: ADD CONSTRUCTOR IN MyArrayListResizable
    Also, fixed a bug in .add(), that only added the value if there was an exception.

--- exercises/src/main/java/org/pg4200/ex02/MyArrayListResizable.java
+++ exercises/src/main/java/org/pg4200/ex02/MyArrayListResizable.java
@@ -4,22 +4,30 @@ import org.pg4200.les02.list.MyArrayList;
 
 public class MyArrayListResizable<T> extends MyArrayList<T> {
 
+    public MyArrayListResizable() {
+        super();
+    }
+
+    public MyArrayListResizable(int capacity) {
+        super(capacity);
+    }
+
     @Override
     public void add(int index, T value) {
 
         if(index < 0) {
             throw new IndexOutOfBoundsException();
         } else if (size == data.length) {
-            Object[] biggerData = new Object[size * 2];
+            Object[] biggerData = new Object[data.length * 2];
 
             for(int i = 0; i < data.length; i++) {
                 biggerData[i] = data[i];
             }
 
             data = biggerData;
-
-            super.add(index, value);
         }
+
+        super.add(index, value);
 
     }
 }
