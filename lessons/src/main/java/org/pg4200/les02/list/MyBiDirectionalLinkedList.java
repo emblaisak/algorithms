60a7c0c539c9bfe2edc7aceaed89e2ec8ffb428a
Embla Mathea <emblamathea@outlook.com>
2021-11-10 17:34:11 +0100

    EX 02-C: ADD DELETE METHOD
    Add method for deleting nodes, but helper method getNode is not operational yet.

--- lessons/src/main/java/org/pg4200/les02/list/MyBiDirectionalLinkedList.java
+++ lessons/src/main/java/org/pg4200/les02/list/MyBiDirectionalLinkedList.java
@@ -1,24 +1,51 @@
 package org.pg4200.les02.list;
 
-import java.util.List;
-
 public class MyBiDirectionalLinkedList<T> implements MyList<T> {
 
-//    Value is stored in Node-object with reference to previous and next element
     private class ListNode {
         T value;
         ListNode previous;
         ListNode next;
-}
+    }
+
+    private ListNode head;
+    private ListNode tail;
+    private int size;
 
     @Override
     public void delete(int index) {
 
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+//        Is this the first node?
+        if (index == 0) {
+            // Move head to the right if there is a node
+            if (head.next != null) {
+                head = head.next;
+                head.previous = null;
+            } else {
+                head = tail = null;
+            }
+//            Is this the last node?
+        } else if (index == (size - 1)) {
+            tail = tail.previous;
+            tail.next = null;
+//            In the last case, we're not working on the head or tail anymore. We need a getNode-method.
+        } else {
+            ListNode target = getNode(index);
+            // Target node is now equal to the next node? I have no idea what happens here.
+            target.previous.next = target.next;
+            target.next.previous = target.previous;
+        }
+
+        size--;
+
     }
 
-    @Override
-    public boolean isEmpty() {
-        return MyList.super.isEmpty();
+    private ListNode getNode(int index) {
+        return new ListNode();
     }
 
     @Override
