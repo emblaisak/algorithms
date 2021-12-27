971ead8ad527f4ce450b901ac951dd34afc79a1a
Embla Mathea <emblamathea@outlook.com>
2021-12-27 18:30:28 +0100

    EX 03-A: ADD NON-OPTIMIZED BUBBLE SORT TEST - PASSED
    Checks if unsorted array is sorted after calling on sort-method.
    Checks if there was a comparison.

--- exercises/src/test/java/org/pg4200/ex03/OptimizedBubbleSortTest.java
+++ exercises/src/test/java/org/pg4200/ex03/OptimizedBubbleSortTest.java
@@ -1,7 +1,30 @@
 package org.pg4200.ex03;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OptimizedBubbleSortTest {
+
     /*
     Need at least one test with array:    ["c", "b", "a", "d", "e", "f"]
+    Optimized sorting should do less than half of the comparisons in the non-optimized one
+    Reach 100% statement coverage
      */
+
+    @Test
+    public void testNonOptimizedStringArray() {
+        String[] actualArray = new String[]{"c", "b", "a", "d", "e", "f"};
+        String[] expectedArray = new String[]{"a", "b", "c", "d", "e", "f"};
+
+        MyStringComparator comparator = new MyStringComparator();
+        OptimizedBubbleSort sorter = new OptimizedBubbleSort();
+
+        int numberOfComparisons = sorter.sort(actualArray, comparator, false);
+        System.out.println("Number of comparisons are: " + numberOfComparisons);
+
+        assertArrayEquals(expectedArray, actualArray);
+        assertTrue(numberOfComparisons > 0);
+    }
+
 }
